40a7b73fbc7806126a7ea129f73c8de3db442769
graj1990 <graysonjarrod@gmail.com>
2025-07-21 14:31:11 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,8 +1,4 @@
-// src/firebase.js
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
-
+// firebase.js
 const firebaseConfig = {
   apiKey: "AIzaSyCLO-NW-9rATuKwhiXPY4XOhUmO7En-vLo",
   authDomain: "whittingtonwings.firebaseapp.com",
@@ -13,6 +9,10 @@ const firebaseConfig = {
   measurementId: "G-WQ60E2HDEF"
 };
 
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export const auth = getAuth(app);
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Set globals
+window.db = firebase.firestore();
+window.auth = firebase.auth();
+window.provider = new firebase.auth.GoogleAuthProvider();
